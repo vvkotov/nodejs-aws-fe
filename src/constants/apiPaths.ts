b6954de91105743734912a90071fb6624f8074fb
Vitaly kotov <vitaly.kotov@bawagpsk.com>
2020-12-07 12:05:19 +0300

    add cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://roh1dpqge3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://f3fw8iyl94.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
