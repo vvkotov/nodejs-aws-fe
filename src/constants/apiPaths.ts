4be0a231c3c8c41eb771334394a0eb09aad8e7c5
Vitaly kotov <vitaly.kotov@bawagpsk.com>
2020-12-12 22:03:33 +0300

    add bff service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://017zzup6h6.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://bybi9urpd3.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://roh1dpqge3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://f3fw8iyl94.execute-api.eu-west-1.amazonaws.com/dev/api',
+  cart: 'https://bybi9urpd3.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
